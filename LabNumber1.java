180ef1baead1496cf6f32e60844ecbc162452016
Tenica Gillam <tmariegillam0103@gmail.com>
2016-08-25 13:40:13 -0400

    initial commit

---
+++ LabNumber1.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class LabNumber1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.println("Welcome to Grand Cisrcus' Room Detail Generator!");
+		
+		
+		
+		System.out.println("enter length");
+		int length = scanner.nextInt();
+		
+		System.out.println("enter width");
+		int width = scanner.nextInt();
+		
+		int area = length * width;
+		
+		int perimeter = 2 * (length * width);
+		
+		System.out.println();
+		
+		System.out.println();
+		
+		System.out.println(area);
+		
+	    System.out.println(perimeter);
+	    
+		
+		
+		
+		
+				
+		
+		
+		
+		
+		
+		
+			
+		
+		
+		
+
+	}
+
+}
