ac908186ecd6d88f13aa834bcc4ecebc5e1f8a9c
Tenica Gillam <tmariegillam0103@gmail.com>
2016-08-31 09:22:09 -0400

    submit LabNumber7

---
+++ LabNumber7.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class LabNumber7 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Scanner sc = new Scanner(System.in);
+
+		String name[] = { "Kim", "Thomas", "Susan", "Kevin", "Tina", "Natasha", "Vincent", "Arthur", "Wendy", "Michelle", "Sharon", "Arthur", "Brian",
+				"Neil", "Aydon", "Ayron", "Oser", "Charlotte", "Leslie"};
+		
+		String homeTown[] = {"Cleveland","Detroit","Ann Arbor", "Eastpointe", "Livonia", "Cincinnati", "Seattle", "Dallas", "Los Angeles", "Milwuakee",
+				"Jackson", "Elkhart", "Birmingham", "Miami", "Boston", "Atlanta", "Toledo", "Phoenix", "Las Vegas", "St. Louis"};
+		
+		String favFood[] = {"Mashed Potatoes", "Hot Dogs", "Maccaroni", "Hamburger", "Pineapple", "Bread", "Steak", "Rice", "Granola", "Chili",
+				"Chicken", "Turkey", "Sandwiches", "Corn", "Nachos", "Pizza", "Cereal", "Cookies", "Pancakes", "Tuna" };
+		
+		System.out.println();
+		System.out.println("Welcome to Grand Circus. Who would you like to learn more about?" + "Enter a number from 1-20.");
+		
+		
+		
+	}
+
+		
+
+
+		
+
+		
+		int number = 0;
+		String choice = "y";
+
+		for (int i = 0; i < name.length; i++) {
+			System.out.println((i + 1) + " " + name[i]);
+
+		
+				
+				
+				for (int j= 0; j < name.length; j++);
+				int i = 0; 
+				i++;
+				if(studentIndex ==i){
+					System.out.priintln(name[j]);
+			//this needs to be worked on 
+	
+				}					
+					
+				
